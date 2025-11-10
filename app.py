27af9824c02f611776564a95d0732d9436e28acd
André Zauli <andresoares@colegiopaulodetarso.com.br>
2025-11-10 19:41:36 +0000

    versao do app

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
-# Exercício 1 — Página inicial (index)
 @app.route('/')
 def index():
     return '<h1>Hello, Flask !!</h1>'
+
+    @app.route('/versao')
+def versao():
+    versao = "1.1.0"
+    return f'App v{versao}'
